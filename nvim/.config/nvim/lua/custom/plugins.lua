fa28fac3b2962c1811d1ba793947713f16373f7d
Harry Anderson <harryanderson9@gmail.com>
2023-01-14 20:33:01 +0000

    init

---
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,17 @@
+return function(use)
+  use "projekt0n/github-nvim-theme"
+  use 'ThePrimeagen/vim-be-good'
+  use {
+    'ThePrimeagen/harpoon',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'hashivim/vim-terraform'
+  use 'mattn/emmet-vim'
+  use "rafamadriz/friendly-snippets"
+    --Debugger
+  use 'mfussenegger/nvim-dap'
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+  use 'nvim-telescope/telescope-dap.nvim'
+  use 'theHamsta/nvim-dap-virtual-text'
+  use 'sbdchd/neoformat'
+end
